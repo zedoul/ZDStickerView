6f228cd204f7bba77d6f6b71f270adffafefb3d8
shyeon.kim <shyeon.kim@scipi.net>
2013-05-29 12:53:11 +0900

    Adding basic structures
    Signed-off-by: shyeon.kim <shyeon.kim@scipi.net>

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 ZDStickerView
 =============
+ZDStickerView is Objective-C module for iOS and offer complete configurability, including movement, resizing, rotation and more, with one finger.
+
+Licence
+========
+The MIT Licence
---
+++ ZDStickerViewApp/ZDStickerViewApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ZDStickerViewApp
+//
+//  Created by zedoul on 5/29/13.
+//  Copyright (c) 2013 scipi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
