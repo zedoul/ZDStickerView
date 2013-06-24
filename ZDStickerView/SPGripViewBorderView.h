4a3cbc5a7734d38668d1c2f68822606d00291347
shyeon.kim <shyeon.kim@scipi.net>
2013-06-24 12:52:28 +0900

    Adding preventsDeleting
    Signed-off-by: shyeon.kim <shyeon.kim@scipi.net>

--- ZDStickerView/SPGripViewBorderView.h
+++ ZDStickerView/SPGripViewBorderView.h
@@ -1,8 +1,7 @@
 //
 //  SPGripViewBorderView.h
-//  ZDStickerViewApp
 //
-//  Created by zedoul on 6/3/13.
+//  Created by Seonghyun Kim on 6/3/13.
 //  Copyright (c) 2013 scipi. All rights reserved.
 //
 
